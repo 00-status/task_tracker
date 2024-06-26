47921edbeb8775b8157621c7ccab4019ba5c89a4
00-status <dedypips@gmail.com>
2024-06-26 11:39:48 -0600

    Added index files.

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import { createRoot } from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const rootDomNode = document.getElementById('app');
+
+if (!rootDomNode) {
+    throw new Error('Could not find App!');
+}
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Hello World</div>
+    }
+]);
+
+const root = createRoot(rootDomNode);
+root.render(<RouterProvider router={router}/>);
